471f1c0b84e0b9e10b7bc6476b4d2b2bb0551638
Vaibhav Patel <vp932712@gmail.com>
2017-12-14 22:03:47 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,8 +14,8 @@ def first_challenge
   }
 
   #your code here
- iceCream = contact["Freddy Mercury"][:"favorite_icecream_flavors"]
-incCream.delete("strawberry")
+ iceCream = contacts["Freddy Mercury"][:"favorite_icecream_flavors"]
+iceCream.delete("strawberry")
   #remember to return your newly altered contacts hash!
   contacts
 end
